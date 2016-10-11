ac3d3d7b9154a5b401607b3f5f8abc5b4d3dff43
unknown <fenghongkang@wind.local>
2016-10-11 11:34:37 +0800

    20161011 init

---
+++ app/src/main/java/com/example/kaifyou/myphone/MyPhoneActivity.java
@@ -0,0 +1,13 @@
+package com.example.kaifyou.myphone;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MyPhoneActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_phone);
+    }
+}
